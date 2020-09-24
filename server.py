a9bf6a263109ea9669df83765a72efbc4f02df94
Ryan Punt <ryan@mirum.org>
2020-09-24 17:15:42 -0500

    it pulses, but it's shaky

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import json
-#import threading
+import threading
+import numpy as np
 from flask import Flask, render_template, jsonify, make_response
 from time import sleep
 import colorsys
@@ -13,6 +14,7 @@ from jsmin import jsmin
 
 app = Flask(__name__)
 
+blinkThread = None
 globalRed = 0
 globalGreen = 0
 globalBlue = 0
@@ -30,25 +32,24 @@ u_width, u_height = unicornhatmini.get_shape()
 #for i in range(0, 360):
 #  hue_to_rgb.append(colorsys.hsv_to_rgb(i / 359.0, 1, 1))
 
-def setColor(r, g, b):
-  setPixels(r, g, b, globalBrightness)
-  unicornhatmini.show()
+#def setColor(r, g, b):
+#  setPixels(r, g, b, globalBrightness)
+#  unicornhatmini.show()
 
-def setPixels(r, g, b, brightness=0.1):
-  global globalBrightness, globalBlue, globalGreen, globalRed
-
-  globalRed = r
-  globalGreen = g
-  globalBlue = b
-
-  if brightness is not None:
-    globalBrightness = brightness
-    unicornhatmini.set_brightness(brightness)
-
-  unicornhatmini.clear()
-  for x in range(u_width):
-    for y in range(u_height):
-      unicornhatmini.set_pixel(x, y, r, g, b)
+#def setPixels(r, g, b, brightness=0.1):
+#  global globalBrightness, globalBlue, globalGreen, globalRed
+#  globalRed = r
+#  globalGreen = g
+#  globalBlue = b
+#
+#  if brightness is not None:
+#    globalBrightness = brightness
+#    unicornhatmini.set_brightness(brightness)
+#
+#  unicornhatmini.clear()
+#  for x in range(u_width):
+#    for y in range(u_height):
+#      unicornhatmini.set_pixel(x, y, r, g, b)
 
 def switchOff():
   global blinkThread, globalBlue, globalGreen, globalRed
@@ -58,7 +59,26 @@ def switchOff():
   if blinkThread is not None:
     blinkThread.do_run = False
   unicornhatmini.clear()
-  unicornhatmini.off()
+  unicornhatmini.show()
+
+def pulsar(r, g, b):
+  global blinkThread, globalBlue, globalGreen, globalRed
+  globalRed = r
+  globalGreen = g
+  globalBlue = b
+  brightness_range = np.arange(0.0, 0.35, 1/60)
+  # print(brightness_range)
+  switchOff()
+  unicornhatmini.set_all(r, g, b)
+  unicornhatmini.show()
+  # print("pulsar with ", r, ", ", g, ", ", b)
+  while True:
+    for x in brightness_range:
+      unicornhatmini.set_brightness(x)
+      time.sleep(1/15)
+    for x in brightness_range[::-1]:
+      unicornhatmini.set_brightness(x)
+      time.sleep(1/15)
 
 @app.route("/")
 def index():
@@ -70,20 +90,30 @@ def index():
 
 @app.route("/api/<command>")
 def action(command):
-  global globalStatus
+  global blinkThread, globalStatus
+
   if command == 'busy':
     globalStatus = command
-    setColor(255, 0, 0)
+    blinkThread = threading.Thread(target=pulsar, args=(255, 0, 0))
+    blinkThread.do_run = True
+    blinkThread.start()
+
   if command == 'free':
     globalStatus = command
-    setColor(0, 255, 0)
+    blinkThread = threading.Thread(target=pulsar, args=(0, 255, 0))
+    blinkThread.do_run = True
+    blinkThread.start()
+
   if command == 'away':
     globalStatus = command
-    setColor(255, 191, 0)
+    blinkThread = threading.Thread(target=pulsar, args=(255, 191, 0))
+    blinkThread.do_run = True
+    blinkThread.start()
+
   if command == 'off':
     globalStatus = command
-    unicornhatmini.clear()
-    unicornhatmini.show()
+    switchOff()
+
   if command == 'status':
     cpu = CPUTemperature()
     return jsonify({
@@ -92,6 +122,7 @@ def action(command):
       'height': u_height,
       'width': u_width
     })
+
   return make_response(jsonify({'status': globalStatus}))
 
 @app.errorhandler(404)
@@ -100,17 +131,3 @@ def not_found(error):
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80, debug=False)
-
-# pulse it, eventually
-#import numpy as np
-#import time
-#import datetime
-#
-#brightness = np.arange(0.0, 0.4, 1/60)
-#while True:
-#  for x in brightness:
-#    print("brightness: ", x)
-#    time.sleep(1/30)
-#  for x in brightness[::-1]:
-#    print("brightness: ", x)
-#    time.sleep(1/15)
