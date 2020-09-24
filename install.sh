86da7dc18331b1eb22760d19cb7bd6db42cb437c
Ryan Punt <ryan@mirum.org>
2020-09-24 16:45:44 -0500

    pi zero requires numpy via apt

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y python3-pip python3-dev
+sudo apt-get install -y python3-pip python3-dev python3-numpy
 sudo pip3 install -r ./requirements.txt
 
 if [ $(sudo raspi-config nonint get_spi) == 0 ]; then
