0e1c91249751a4dc0381413a75137d785ef96c44
Ryan Punt <ryan@mirum.org>
2021-11-30 17:15:30 -0600

    add service installation to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 sudo apt install -y python3-pip python3-dev python3-numpy
 sudo pip3 install -r ./requirements.txt
 
+sudo cp freebusy.service /lib/systemd/system/freebusy.service
+sudo systemctl daemon-reload
+sudo systemctl enable freebusy.service
+
 if [ $(sudo raspi-config nonint get_spi) == 0 ]; then
   echo "SPI is enabled"
 else
